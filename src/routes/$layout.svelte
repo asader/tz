2b94d2920ddc3977cc9b8d1b40645f7b4c2bfb99
asader <oqoporu@gmail.com>
2020-12-08 17:30:21 +0300

    Init

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<slot />
+
+<!-- <div>
+  {#key $page.path}
+    <main out:fade in:fade={{ delay: 400, duration: 400 }}>
+      <slot />
+    </main>
+  {/key}
+</div> -->
